58d58c9410a66e730dc7b624efeead110558c556
purelind <purelind@gmail.com>
2022-01-13 18:36:54 +0800

    remove useless code

--- tipipeline/pipeline-templates/daily-check.groovy
+++ tipipeline/pipeline-templates/daily-check.groovy
@@ -1,12 +1,12 @@
-// properties([
-//         parameters([
-//                 string(
-//                         defaultValue: '',
-//                         name: 'PIPELINE_YAML',
-//                         trim: true
-//                 ),
-//         ])
-// ])
+properties([
+        parameters([
+                string(
+                        defaultValue: '',
+                        name: 'PIPELINE_YAML',
+                        trim: true
+                ),
+        ])
+])
 
 def get_sha(repo,branch) {
     sh "curl -s ${FILE_SERVER_URL}/download/builds/pingcap/ee/get_hash_from_github.py > gethash.py"
@@ -15,24 +15,19 @@ def get_sha(repo,branch) {
 
 common = {}
 commitID = ""
-// PipelineSpec pipelineSpec =  new PipelineSpec()
+
 node("${GO1160_BUILD_SLAVE}") {
     container("golang") {
-        // checkout scm
-        // common = load "tipipeline/common.groovy"
-
+        checkout scm
+        common = load "tipipeline/common.groovy"
         // sh """
-        // wget https://raw.githubusercontent.com/purelind/jenkins-templates/purelind/refactor-init-test-fix/tipipeline/common.groovy
+        // wget http://fileserver.pingcap.net/download/cicd/debug/common.groovy
         // """
-        sh """
-        wget http://fileserver.pingcap.net/download/cicd/debug/common.groovy
-        """
-        common = load "common.groovy"
+        // common = load "common.groovy"
 
         pipelineSpec = common.loadPipelineConfig(PIPELINE_YAML)
         commitID = get_sha(pipelineSpec.repo,pipelineSpec.defaultRef)
     }
 }
 
-// pipelineSpec = comloadPipelineConfig(PIPELINE_YAML)
 common.runPipeline(pipelineSpec, "daily", pipelineSpec.defaultRef, String commitID, "")
