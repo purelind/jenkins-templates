c7f0eb150e177bd5625091b5582a4c284b666352
purelind <purelind@gmail.com>
2023-05-22 10:42:34 +0800

    chore: update nfs

--- atom-jobs/atom-gosec.groovy
+++ atom-jobs/atom-gosec.groovy
@@ -74,10 +74,10 @@ def run_with_pod(Closure body) {
                     ),
             ],
             volumes: [
-                    nfsVolume(mountPath: '/home/jenkins/agent/ci-cached-code-daily', serverAddress: '172.16.5.22',
-                            serverPath: '/mnt/ci.pingcap.net-nfs/git', readOnly: false),
-                    nfsVolume(mountPath: '/nfs/cache', serverAddress: '172.16.5.22',
-                            serverPath: '/mnt/ci.pingcap.net-nfs', readOnly: false),
+                    nfsVolume(mountPath: '/home/jenkins/agent/ci-cached-code-daily', serverAddress: "${NFS_SERVER_ADDRESS}",
+                            serverPath: '/data/nvme1n1/nfs/git', readOnly: false),
+                    nfsVolume(mountPath: '/nfs/cache', serverAddress: "${NFS_SERVER_ADDRESS}",
+                            serverPath: '/data/nvme1n1/nfs', readOnly: false),
             ],
     ) {
         node(label) {
